fb994cf0507164d8e76333fa80a4b98ab1c18d25
tevfikakts <tevaktas@gmail.com>
2026-05-12 13:32:20 +0300

    build(faz1): proje iskeleti, docker orkestrasyonu ve pgvector altyapisi kuruldu

---
+++ src/main/java/com/tevfik/kentgozu/KentgozuApplication.java
@@ -0,0 +1,13 @@
+package com.tevfik.kentgozu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KentgozuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KentgozuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tevfik/kentgozu/KentgozuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tevfik.kentgozu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KentgozuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
